17191c5511b106a6415735cd6197eb34acb1dde5
Brad Wilson <brad@lucky-dip.net>
2011-06-29 11:25:07 +1000

    add processor that creates fingerprint files (using md5)

---
+++ lib/quinc/processors/fingerprinter.rb
@@ -0,0 +1,38 @@
+require 'digest'
+require 'fileutils'
+
+module Quinc
+  module Processors
+
+    class Fingerprinter
+      attr_accessor :marker
+
+      def initialize(marker = "-fp-")
+        self.marker = marker
+      end
+
+      def process(files)
+        fingerprints = files.map { |f| fingerprinted_file_for(f) }
+        files + fingerprints
+      end
+
+      def fingerprinted_file_for(f)
+        dest = filename_for(f)
+        FileUtils.copy(f, dest) unless File.exists?(dest)
+        dest
+      end
+
+      def digest_for(f)
+        Digest::MD5.file(f)
+      end
+
+      def filename_for(f)
+        extname = File.extname(f)
+        basename = f.chomp(extname)
+
+        "#{ basename }#{ marker }#{ digest_for(f) }#{ extname }"
+      end
+    end
+
+  end
+end
